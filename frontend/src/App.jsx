538c32324af9549245bd97f1f50b213f336e1214
mahendrajangid2583 <mahendrajangid2583@gmail.com>
2025-08-30 22:32:02 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,8 @@
-import { react } from 'react'
+import React from 'react'
 
-
-import './App.css'
-
-function App() {
- 
-
+const App = () => {
   return (
-    <>
-<<<<<<< HEAD
-      <div className=' text-3xl font-bold underline bg-[#f0f0f0]'>
-=======
-      <div className="bg-amber-400">
->>>>>>> 9f0b4a0e2527591d788d4cdcbe64f7793a151c1d
-         hello world
-        
-      </div>
-    </>
+    <div>App</div>
   )
 }
 
