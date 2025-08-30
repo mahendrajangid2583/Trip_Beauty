d894e07adfb73ebb41d93cbb25b34c8929a66f9a
aashutoshsingh <aashutoshsingh226@gmail.com>
2025-08-30 22:02:24 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { react } from 'react'
+
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
+      <div className="bg-amber-400">
          hello world
         
       </div>
