06b1ac9bc5f0a2515fd2130dab524466e848f0e8
aashutoshsingh <aashutoshsingh226@gmail.com>
2025-08-30 22:16:51 +0530

    fixed error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,14 @@
-import { react } from 'react'
+import { useState } from 'react'
 
-
-import './App.css'
 
 function App() {
- 
+  const [count, setCount] = useState(0)
 
   return (
     <>
       <div className="bg-amber-400">
          hello world
-        
+        <button onClick={() => setCount(count + 1)}>Increment</button>
       </div>
     </>
   )
