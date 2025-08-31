c093e76476d3bb07425893727262af90799d02c9
aashutoshsingh <aashutoshsingh226@gmail.com>
2025-08-31 07:16:26 +0530

    added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from "./pages/Home";
+
+const appRouter=createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  }
+]);
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
-  return (
-    <div className="App w-full text-center mt-10">
-      <h1>Hello World</h1>
-      <button onClick={() => setCount((count) => count + 1)} className="p-2 mt-4 bg-black text-white rounded-lg border-1 border-white hover:bg-gray-600 font-bold">
-        count is {count}
-      </button>
-    </div>
+  return (<RouterProvider router={appRouter}/>
   );
 }
 
